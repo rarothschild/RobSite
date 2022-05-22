eab61fd90dfc5a300d2b28208ec782cff2fed5df
rrothschild <rarothsc@gmail.com>
2022-05-22 09:29:15 -0700

    styles updates and url fixes

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('projects/',views.projects,name='projects'),
+    path('projects/overview',views.projects_overview,name='projects_overview'),
+    path('projects/overview',views.projects_trading,name='projects_trading'),
+    path('projects/overview',views.projects_landtrack,name='projects_landtrack'),
     path('resume/',views.resume,name='resume'),
     path('contact/',views.contact,name='contact'),
     ]
--- main/views.py
+++ main/views.py
@@ -7,17 +7,20 @@ import os
 def home(request):
     return render(request,'main/home.html')
 
-def projects(request):
-    return render(request,'main/projects.html')
-
 def resume(request):
     return render(request,'main/resume.html')
 
 def contact(request):
     return render(request,'main/contact.html')
 
-def project_overview(request):
-    return render(request,'main/contact.html')
+def projects_overview(request):
+    return render(request,'main/projects_overview.html')
+
+def projects_trading(request):
+    return render(request,'main/projects_trading.html')
+
+def projects_landtrack(request):
+    return render(request,'main/projects_landtrack.html')
 
 def show_pdf(request):
     filepath = os.path.join('static','images', 'RothschildResume.pdf')
