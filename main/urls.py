eb8c912cd3073719962ef181acc31e26ffa91d80
rrothschild <rarothsc@gmail.com>
2022-05-22 19:33:52 -0700

    Refactoring and updating styles

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('projects/overview',views.projects_overview,name='projects_overview'),
-    path('projects/overview',views.projects_trading,name='projects_trading'),
-    path('projects/overview',views.projects_landtrack,name='projects_landtrack'),
+    path('projects/trading',views.projects_trading,name='projects_trading'),
+    path('projects/landtrack',views.projects_landtrack,name='projects_landtrack'),
     path('resume/',views.resume,name='resume'),
     path('contact/',views.contact,name='contact'),
     ]
